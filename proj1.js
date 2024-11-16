bd73a37ae4acea94acdc130b92a368f7f48ac87f
vivek11924 <151419188+vivek11924@users.noreply.github.com>
2024-11-16 20:35:50 +0530

    Add files via upload

---
+++ proj1.js
@@ -0,0 +1,4 @@
+// Simple JavaScript function to show an alert when the "Learn More" button is clicked
+function showAlert() {
+    alert("Thank you for your interest! For more details, please contact our admission office.");
+}
